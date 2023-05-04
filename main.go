1f104ca90c9ff9722d82adc0bac41b9a3e0f1f65
phillipashe <ashephillip@gmail.com>
2023-05-04 19:32:55 -0400

    add a timer

--- main.go
+++ main.go
@@ -1,8 +1,18 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"time"
+)
 
 func main() {
+
+	startTimer := time.Now()
+	// defer runs after the function exits
+	// this uses anon func but can pass a named func too
+	defer func() {
+		fmt.Println(time.Since(startTimer))
+	}()
 
 	names := [4]string{"Thomas", "Caleb", "Shelby", "Phillip"}
 	shootLaser(names)
